462080c48eb331f87f45ea3b3943251ca17d657e
João Vitor Rodrigues Baptista <jvrbaptista@live.com>
2019-12-26 14:21:27 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,2 @@
 #!/usr/bin/env bash
-
-#RETRIES=20
-#
-#until psql -h $PG_HOST -U $PG_USER -d $PG_DATABASE -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
-#  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
-#  sleep 1
-#done
-
-#RETRIES=20
-#
-#until psql -h postgres -U postgres -d qbot -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
-#  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
-#  sleep 1
-#done
-
-python helloworld_project/manage.py migrate
-python helloworld_project/manage.py collectstatic --no-input
-python helloworld_project/manage.py runserver 0.0.0.0:8000
-
+docker-compose up -d
